1bfcfcc0f57725b260a9b4f6c94217f430bd1645
kaustubh <2116373+kaustubh-d@users.noreply.github.com>
2025-04-06 12:59:55 +0530

    U2/f2 f4 f6 to main (#23)
    * f2: u2 changes A2 (#11) (#12)
    
    * u2: f4 (#15) (#17)
    
    * u2: f6 (#19) (#21)

--- src/main.go
+++ src/main.go
@@ -6,6 +6,9 @@ func main() {
 	fmt.Println("Hello World!!")
 
 	fmt.Println("source line A1!!")
-	fmt.Println("source line A2!!")
-	fmt.Println("source line A3!!")
+	fmt.Println("source line A2.1!!")
+	fmt.Println("source line A2.2!!")
+	fmt.Println("source line A3.1!!")
+	fmt.Println("source line A3.2!!")
+	u2f6sayshello()
 }
